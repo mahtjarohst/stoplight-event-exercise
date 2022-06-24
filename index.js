14d4e0d3d5d1a73e0479e9a85eb8013be7031b2a
mahtjarohst <matthewrust21@gmail.com>
2022-06-24 15:53:16 -0500

    Required functionality complete

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-(function() {
-  'use strict';
+let stopButton = document.getElementById("stopButton");
+let slowButton = document.getElementById("slowButton");
+let goButton = document.getElementById("goButton");
 
-  // YOUR CODE HERE
-})();
+stopButton.addEventListener("click", () => {
+    stopLight.classList.toggle("stop");
+});
+slowButton.addEventListener("click", () => {
+    slowLight.classList.toggle("slow");
+});
+goButton.addEventListener("click", () => {
+    goLight.classList.toggle("go");
+});
+stopButton.addEventListener("mouseenter", () => {
+    console.log(`Entered ${stopButton.innerText}`);
+});
+
+[stopButton, slowButton, goButton].forEach((button) => {
+    button.addEventListener("mouseenter", () => {
+        console.log(`Entered ${event.target.innerText} button `);
+    });
+    button.addEventListener("mouseleave", (event) => {
+        console.log(`Left ${event.target.innerText} button`);
+    });
+});
